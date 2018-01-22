b4b060fbb7c9fd4d05ea673acfbe82b05559c56d
Dmitry <35579597+Ariorh1337@users.noreply.github.com>
2018-01-22 02:39:49 +0100

    Create index.js
    lets do this!

---
+++ index.js
@@ -0,0 +1,59 @@
+const Command = require('command'), fs = require('fs'), path = require('path');
+var chaise = false, log = '\r\n' + 'Start log';
+const dir = path.join(__dirname, '..', '..', '..', 'logs');
+var playerId = ['','','','',''], clas = ['','','','',''], name = ['','','','','']
+if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+module.exports = function FlyMore(dispatch) {
+	loot_file = path.join(dir, 'log-ABNORMAL-' + Date.now() + '.log');
+	const command = Command(dispatch);
+	const chatHandler = event => {
+		const message = event.message.replace(/(<([^>]+)>)/ig, '');
+		switch (message) {
+			case '!savelog':
+				write(loot_file, log);
+				log = '\r\n' + 'Start log';
+				return false;
+			case '!marker':
+				log = log + '\r\n' + '[MARKER!!!]';
+				return false;
+		}
+	}
+	var abnormal
+	dispatch.hook('C_WHISPER', 1, chatHandler);
+	dispatch.hook('C_CHAT', 1, chatHandler);
+	dispatch.hook('S_PARTY_MEMBER_LIST', 6, event => {
+		party = {
+			raid: event.raid,
+		};
+		if (party.raid == false) {
+			z = event.members.length
+			//console.log('%j', 'Thats what are you looking for: ' + event.members[0].name);
+			for (i = 0; i < z; i++) {
+				playerId[i] = event.members[i].playerId;
+				clas[i] = event.members[i].class;
+				name[i] = event.members[i].name;
+			}
+		};
+	});
+	dispatch.hook('S_PARTY_MEMBER_ABNORMAL_ADD', 3, event => {
+		abnormal = {
+			playerId: event.playerId,
+			id: event.id
+		};
+		var time = new Date();
+		var timeStr = ("0" + time.getHours()).slice(-2) + ":" + ("0" + time.getMinutes()).slice(-2) + ":" + ("0" + time.getSeconds()).slice(-2);
+		for (i = 0; i < z; i++) {
+			if (abnormal.playerId == playerId[i]) {
+				playerName = name[i];
+				playerClass = clas[i]
+			}
+		}
+		if (playerClass == 1) {playerClass = 'Knight'} else if (playerClass == 2) { playerClass = 'Slayer' } else if (playerClass == 3) {
+			playerClass = 'Berserk' } else if (playerClass == 4) { playerClass = 4 } else if (playerClass == 5) { playerClass = 'Archer' } else if (playerClass == 6) {
+			playerClass = 6 } else if (playerClass == 7) { playerClass = 7 } else if (playerClass == 8) { playerClass = 8 } else if (playerClass == 9) {
+			playerClass = 'Engineer' } else if (playerClass == 10) { playerClass = 'Crusher' } else if (playerClass == 11) { playerClass = 'Shinobi' } else if (playerClass == 0) {
+			playerClass = 'Warrior' } else if (playerClass == 12) { playerClass = 'Valkyrie' }
+		log = log + '\r\n' + "[" + timeStr + "]" + ' Name: ' + playerName + ' Class: ' + playerClass + ' PlayerID: ' + abnormal.playerId + ' ID: ' + abnormal.id;
+	});
+	function write(file, data) { fs.appendFileSync(file, data) }
+}
