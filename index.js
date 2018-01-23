c9442736ec7605f3180ee15cafb141fef05734bb
Dmitry <35579597+Ariorh1337@users.noreply.github.com>
2018-01-23 22:51:04 +0100

    repared

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ module.exports = function FlyMore(dispatch) {
 			raid: event.raid,
 		};
 		if (party.raid == false) {
-			z = event.members.length
+			pm = event.members.length
 			//console.log('%j', 'Thats what are you looking for: ' + event.members[0].name);
-			for (i = 0; i < z; i++) {
+			for (i = 0; i < pm; i++) {
 				playerId[i] = event.members[i].playerId;
 				clas[i] = event.members[i].class;
 				name[i] = event.members[i].name;
@@ -43,30 +43,35 @@ module.exports = function FlyMore(dispatch) {
 		};
 		var time = new Date();
 		var timeStr = ("0" + time.getHours()).slice(-2) + ":" + ("0" + time.getMinutes()).slice(-2) + ":" + ("0" + time.getSeconds()).slice(-2);
-		for (i = 0; i < z; i++) {
-			if (abnormal.playerId == playerId[i]) {
+		for (i = 0; i < pm; i++) {
+			if (abnormal.playerId === playerId[i]) {
 				playerName = name[i];
-				playerClass = clas[i]
+				playerClass = clas[i];
+				break;
 			}
 		}
-		if (playerClass == 1) {playerClass = 'Knight'} else if (playerClass == 2) { playerClass = 'Slayer' } else if (playerClass == 3) {
-			playerClass = 'Berserk' } else if (playerClass == 4) { playerClass = 4 } else if (playerClass == 5) { playerClass = 'Archer'
-			} else if (playerClass == 6) {playerClass = 6 } else if (playerClass == 7) { playerClass = 'Mystic' } else if (playerClass == 8) {
-			playerClass = 8 } else if (playerClass == 9) {playerClass = 'Engineer' } else if (playerClass == 10) { playerClass = 'Crusher'
-			} else if (playerClass == 11) { playerClass = 'Shinobi' } else if (playerClass == 0) {playerClass = 'Warrior'
-			} else if (playerClass == 12) { playerClass = 'Valkyrie' }
+		//if (playerClass == 0) {playerClass = 0}//'Warrior' ???
+		//if (playerClass == 1) {playerClass = 1}//'Knight' ???
+		if (playerClass == 2) {playerClass = 'Slayer'}//'Убийца''Slayer'
+		//if (playerClass == 3) {playerClass = 3}//'Berserk' ???
+		//if (playerClass == 4) { playerClass = 4 }
+		//if (playerClass == 5) { playerClass = 5}//'Archer' ???
+		//if (playerClass == 6) {playerClass = 6 }
+		//if (playerClass == 7) { playerClass = 7 }//'Mystic' ???
+		if (playerClass == 8) {playerClass = 'Reaper'}//'Жнец''Reaper'
+		if (playerClass == 9) {playerClass = 'Engineer'}//'Инженер''Engineer'
+		//if (playerClass == 10) { playerClass = 10}//'Crusher' ???
+		if (playerClass == 11) { playerClass = 'Shinobi'}//'Шиноби''Shinobi'
+		if (playerClass == 12) { playerClass = 'Valkyrie'}//'Валькирия''Valkyrie'
 		identfied = 'none';
 		z = Identified.unknown.length;
 		for (i = 0; i < z; i++) {
-			if (abnormal.id == Identified.unknown[i]) { identfied = 'unknown'; break;}
-		}
-		z = Identified.Debuff.length;
-		for (i = 0; i < z; i++) {
-			if (abnormal.id == Identified.Debuff[i]) { identfied = 'Debaff'; break;}
+			if (abnormal.id == Identified.unknown[i]) { identfied = 'unknown'; break;
+			} else if (abnormal.id == Identified.Debuff[i]) { identfied = 'Debaff'; break;}
 		}
 		if (identfied == 'none') {
 			log = log + '\r\n' + "[" + timeStr + "]" + ' Name: ' + playerName + ' Class: ' + playerClass + ' \
-PlayerID: ' + abnormal.playerId + ' ID: ' + abnormal.id + ' Identified: ' + identfied;
+PlayerID: ' + abnormal.playerId + ' ID: ' + abnormal.id;
 		} else {
 			//log = log + '\r\n' + "[" + timeStr + "]" + ' Name: ' + playerName + ' Class: ' + playerClass + ' \
 //PlayerID: ' + abnormal.playerId + ' ID: ' + abnormal.id + ' Identified: ' + identfied;
